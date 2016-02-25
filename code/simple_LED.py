062842e881c79d7e23ffece23798d0c5b821115a
n03328818 <delimamg1@hawkmail.newpaltz.edu>
2016-02-24 22:03:08 -0500

    Assignment 3

---
+++ code/simple_LED.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18,GPIO.OUT)
+
+def Blink():
+ while True:
+ 	GPIO.output(18,True)
+ 	time.sleep(0.2)
+ 	GPIO.output(18,False)
+ 	time.sleep(0.2)
+	GPIO.output(18,True)
+	time.sleep(0.2)
+	GPIO.output(18,False)
+	time.sleep(0.2)
+	GPIO.output(18,True)
+	time.sleep(0.2)
+	GPIO.output(18,False)
+	time.sleep(5)
+	GPIO.output(18,True)
+        time.sleep(0.2)
+	GPIO.output(18,False)
+        time.sleep(0.2)
+        GPIO.output(18,True)
+        time.sleep(0.2)
+        GPIO.output(18,False)
+        time.sleep(0.2)
+        GPIO.output(18,True)
+        time.sleep(0.2)
+        GPIO.output(18,False)
+	time.sleep(0.2)
+	GPIO.output(18,True)
+	time.sleep(0.2)
+	GPIO.output(18,False)
+        time.sleep(5)
+ print "done!!"
+ GPIO.cleanup()
+Blink()
